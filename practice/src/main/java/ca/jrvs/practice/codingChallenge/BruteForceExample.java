9dc732caf3677bf97640dbc9f0826c148b3e39a1
Ivan Zapanta <ivanzapanta@gmail.com>
2025-01-08 11:56:04 -0500

    coding challenges

--- practice/src/main/java/ca/jrvs/practice/codingChallenge/BruteForceExample.java
+++ practice/src/main/java/ca/jrvs/practice/codingChallenge/BruteForceExample.java
@@ -5,6 +5,7 @@ public class BruteForceExample {
     int[] nums = {3, 1, 4, 1, 5, 9};
     int max = findMax(nums);
     System.out.println("The maximum number is: " + max);
+
   }
 
   // Brute force method to find the maximum number in the array
