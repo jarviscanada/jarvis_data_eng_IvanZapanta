7d9c8aea4095e0cbfd2a0bd5354a222f3c58998d
Ivan Zapanta <ivanzapanta@gmail.com>
2025-01-27 15:53:02 -0500

    added fetch,add,delete

---
+++ frontend/trading-ui/src/component/TraderList/TraderList.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { Table } from "antd";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "antd/dist/antd.css";
+import "./TraderList.scss";
+import { useState, useEffect } from "react";
+import TraderListData from "./TraderListData.json";
+import { faTrashAlt as deleteIcon } from "@fortawesome/free-solid-svg-icons";
+
+function TraderList(props) {
+  // Initialization of columns for table
+  const columns = [
+    {
+      title: "First Name",
+      dataIndex: "firstName",
+      key: "firstName",
+      sorter: (a, b) => a.firstName.localeCompare(b.firstName), // Sort by first name (antd)
+    },
+    {
+      title: "Last Name",
+      dataIndex: "lastName",
+      key: "lastName",
+      sorter: (a, b) => a.lastName.localeCompare(b.lastName), // Sort by last name (antd)
+    },
+    {
+      title: "Email",
+      dataIndex: "email",
+      key: "email",
+    },
+    {
+      title: "Date of Birth",
+      dataIndex: "dob",
+      key: "dob",
+      sorter: (a, b) => new Date(a.dob) - new Date(b.dob), // Sort by date of birth
+    },
+    {
+      title: "Country",
+      dataIndex: "country",
+      key: "country",
+    },
+    {
+      title: "Actions",
+      dataIndex: "actions",
+      key: "actions",
+      render: (text, record) => (
+        <div className="trader-delete-icon">
+          <FontAwesomeIcon
+            icon={deleteIcon}
+            onClick={() => props.onTraderDeleteClick(record.id)}
+          />
+        </div>
+      ),
+    },
+  ];
+
+  const [tableColumns, setTableColumns] = useState(columns);
+  const [dataSource, setDataSource] = useState([]);
+
+  useEffect(() => {
+    const dataSource = TraderListData;
+    setDataSource(dataSource);
+  });
+
+  return (
+    <Table dataSource={dataSource} columns={tableColumns} pagination={false} />
+  );
+}
+
+export default TraderList;
--- frontend/trading-ui/src/page/Dashboard/Dashboard.jsx
+++ frontend/trading-ui/src/page/Dashboard/Dashboard.jsx
@@ -1,12 +1,124 @@
 import React from "react";
 import "./Dashboard.scss";
-import NavBar from "../../component/Navbar/NavBar";
+import NavBar from "../../component/NavBar/NavBar";
+import TraderList from "../../component/TraderList/TraderList";
+import TraderListData from "../../component/TraderList/TraderListData.json";
+import { Input, DatePicker, Modal, Button, Form } from "antd";
+import axios from "axios";
+import {
+  createTraderUrl,
+  deleteTraderUrl,
+  tradersUrl,
+} from "../../util/constants";
+import "antd/dist/antd.min.css";
+import { useEffect, useState } from "react";
 
 function Dashboard(props) {
+  const [state, setState] = useState({
+    isModalVisible: false,
+    traders: [],
+  });
+
+  const getTraders = () => {
+    setState({
+      ...state,
+      traders: [...TraderListData],
+    });
+  };
+
+  const showModal = () => {
+    setState({
+      ...state,
+      isModalVisible: true,
+    });
+  };
+
+  const handleOk = async () => {
+    await getTraders();
+
+    setState({
+      ...state,
+      isModalVisible: false,
+      firstName: null,
+      lastName: null,
+      dob: null,
+      country: null,
+      email: null,
+    });
+  };
+
+  const onInputChange = (field, value) => {
+    setState({
+      ...state,
+      [field]: value,
+    });
+  };
+
+  const handleCancel = () => {
+    // close model here
+  };
+
+  useEffect(() => {
+    getTraders();
+  }, []);
+
+  // onTraderDelete method implementation is empty for now
+  // This function should be written above the return of the component
+  const onTraderDelete = async (id) => {
+    //delete trader logic
+    console.log("Trader " + id + " is deleted.");
+    await getTraders();
+  };
+
   return (
     <div className="dashboard">
       <NavBar />
-      <div className="dashboard-content">Dashboard Content</div>
+      <div className="dashboard-content">
+        <div className="title">
+          Dashboard
+          <div className="add-trader-button">
+            <Button onClick={showModal}>Add New Trader</Button>
+            <Modal
+              title="Add New Trader"
+              okText="Submit"
+              visible={state.isModalVisible}
+              onOk={handleOk}
+              onCancel={handleCancel}
+            >
+              <Form layout="vertical">
+                <div className="add-trader-form">
+                  <div className="add-trader-field">
+                    <Form.Item label="First Name">
+                      <Input
+                        allowClear={false}
+                        placeholder="John"
+                        onChange={(event) =>
+                          onInputChange("firstName", event.target.value)
+                        }
+                      />
+                    </Form.Item>
+                  </div>
+                  <div className="add-trader-field">... last name ...</div>
+                  <div className="add-trader-field">... email ...</div>
+                  <div className="add-trader-field">... country ...</div>
+                  <div className="add-trader-field">
+                    <Form.Item label="Date of Birth">
+                      <DatePicker
+                        style={{ width: "100%" }}
+                        placeholder=""
+                        onChange={(date, dateString) =>
+                          onInputChange("dob", date.format("yyyy-MM-DD"))
+                        }
+                      />
+                    </Form.Item>
+                  </div>
+                </div>
+              </Form>
+            </Modal>
+          </div>
+        </div>
+        <TraderList onTraderDeleteClick={onTraderDelete} />
+      </div>
     </div>
   );
 }
